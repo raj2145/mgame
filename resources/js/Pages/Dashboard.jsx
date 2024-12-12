5eda87af72b09f016350742e8a280071751cbc5e
rajivAGT <rajeevsingh.agt@gmail.com>
2024-12-12 23:16:09 +0530

    Your commit message

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,48 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <nav className="-mx-3 flex flex-1 justify-end">
+                {auth.user ? (
+                    <Link
+                        href={route('dashboard')}
+                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Dashboard
+                    </Link>
+                ) : (
+                    <>
+                        <Link
+                            href={route('login')}
+                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            href={route('register')}
+                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
+            </nav>
+            }
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            You're logged in!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
